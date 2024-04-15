5b64b9793df743d78857df5fdb8743f12ce33b93
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-04-15 20:55:46 +0200

    Fixed error causing truncation of log entries.

--- migrateElog2Olog.py
+++ migrateElog2Olog.py
@@ -97,17 +97,17 @@ def format_tables(input_data):
     # Add a neline in the beginning to have the same find string even for the first line
     input_data = "\n" + input_data
     input_data_length = len(input_data)
-    print ("DEBUG (format_tables): input_data_length:"+str(input_data_length)+" data: \n"+input_data)
+    #print ("DEBUG (format_tables): input_data_length:"+str(input_data_length)+" data: \n"+input_data)
     while main_index < input_data_length:
         indx = input_data.find("\n|", main_index)
-        print ("DEBUG(format_tables):  main_ind: ", main_index)
+        #print ("DEBUG(format_tables):  main_ind: ", main_index)
         if (indx < 0):
             output_data = output_data + input_data[main_index:]
-            print ("DEBUG(format_tables): no more tables")
+            #print ("DEBUG(format_tables): no more tables")
             break
         else:
             output_data += input_data[main_index:indx]
-            print ("DEBUG(format_tables): text b4 the table: indx: "+str(indx)+" output_data: \n"+output_data)
+            #print ("DEBUG(format_tables): text b4 the table: indx: "+str(indx)+" output_data: \n"+output_data)
             main_index = indx
             eol_indx = input_data.find("\n", main_index+1)
             columns = input_data.count("|",main_index,eol_indx)
@@ -116,13 +116,13 @@ def format_tables(input_data):
             n = columns
             while last_bar_indx != -1 and n > 0:
                 last_bar_indx = input_data.find("|", last_bar_indx+1)
-                print("DEBUG(format_tables): n, last_bar_indx: ", n, last_bar_indx)
+                #print("DEBUG(format_tables): n, last_bar_indx: ", n, last_bar_indx)
                 n -= 1
-            print("DEBUG(format_tables): textefter last bar ", input_data[last_bar_indx+1:eol_indx])  
+            #print("DEBUG(format_tables): textefter last bar ", input_data[last_bar_indx+1:eol_indx])  
             if input_data[last_bar_indx+1:eol_indx].isspace():
                 columns -= 1
-            print("DEBUG(format_tables): title line: ", input_data[main_index:eol_indx])
-            print("DEBUG(format_tables): found columns: ", columns)
+            #print("DEBUG(format_tables): title line: ", input_data[main_index:eol_indx])
+            #print("DEBUG(format_tables): found columns: ", columns)
             # copy the first row (title with the terminating \n)
             output_data += input_data[main_index:eol_indx+1]
             #print ("DEBUG(format_tables): output_data: ", output_data)
@@ -131,13 +131,13 @@ def format_tables(input_data):
             for i in range(columns):
                 output_data += "|-"
             #output_data += "\n"
-            print("DEBUG(format_table): Separator line: ", output_data[main_index:])
+            #print("DEBUG(format_table): Separator line: ", output_data[main_index:])
             # copy rest of the table
             while input_data.find("\n|", main_index) >= 0:
                 line = input_data[main_index:input_data.find("\n", main_index+1)]
                 output_data += line
                 main_index += len (line)
-    print("DEBUG (format_tables): output_data: ", output_data)
+    #print("DEBUG (format_tables): output_data: ", output_data)
     return output_data
     
 # Change the wiki formatting to commonmark (text outside the code block)
@@ -158,18 +158,19 @@ def wiki2commonmark(input_data):
     commonmark0 = input_data.replace("\n", "  \n")
     commonmark=""
     main_index = 0
-    input_data_length = len(input_data)
-    #print ("DEBUG: inp_data_length: " ,input_data_length)
+    input_data_length = len(commonmark0)
+    print ("DEBUG(wiki2commonmark): inp_data_length: " ,input_data_length)
     while main_index < input_data_length:
-        #print("DEBUG: main_index: ", main_index)
+        print("DEBUG(wiki2commonmark): main_index: "+str(main_index)+"   Data@main_index: "+commonmark0[main_index:main_index+40])
         # Insert <newline> as the first character in order to make the search for tags
         # the same in the beginiing of the document as in hte rest of it
         indx, length, block = replace_tags("\n"+commonmark0[main_index:], "\n{{", "\n}}  \n", "\n```\n", "\n```\n")
-        #print ("DEBUG: block_length, indx: " ,length, indx)
+        print ("DEBUG(wiki2commonmark): block_length, indx: " ,length, indx)
         if (indx >= 0):
             commonmark = commonmark + replace_formatting(commonmark0[main_index:main_index+indx])
             commonmark = commonmark + block
-            main_index = main_index + length + indx
+            main_index = main_index + length + indx - 1
+            print("DEBUG(wiki2commonmark): main_index after the last code block:", main_index)
         else:
             commonmark = commonmark + replace_formatting(commonmark0[main_index:])
             break
@@ -244,12 +245,12 @@ def create_log_entry_with_attachments(api_endpoint, logbook, owner, authors, tim
         attachment = ('files', ('FREIAlogo-v3_March19.png', open('FREIAlogo-v3_March19.png', 'rb'), 'image/png'))
         attchmntId = str(uuid.uuid4())
 
-        print (attachment[1][0])
-        print (attachment[0] + ": ", attachment[1])
+        #print (attachment[1][0])
+        #print (attachment[0] + ": ", attachment[1])
         # Prepare the log entry payload as JSON
         json_filename = json.dumps({"filename": attachment[0]})
         json_descr = json.dumps({"fileMetadataDescription": attachment[2]})
-    print (log_entry)
+    #print (log_entry)
 
     # Prepare the log entry payload as JSON
     json_data = json.dumps(log_entry)
@@ -275,7 +276,7 @@ def create_log_entry_with_attachments(api_endpoint, logbook, owner, authors, tim
         'Content-Type': multipart_data.content_type
     }
     print("Content-Type " + multipart_data.content_type)
-    print(multipart_data)
+    #print(multipart_data)
     try:
         response = requests.put(api_endpoint, headers=headers, data=multipart_data, auth=HTTPBasicAuth(owner, dict[owner]))
         if response.status_code == 200:
@@ -351,7 +352,7 @@ def main():
         #print (dir(content))
         # convert from wiki markup to commonmark 
         commonmark = wiki2commonmark (content)
-        print (commonmark)
+        print ("=========\n"+commonmark+"=========")
         create_log_entry_with_attachments(url, logbook, owner, authors, timestamp, title, level, tags, commonmark, attachment)
         #print("Logbook:", logbook)
     except Exception as e:
